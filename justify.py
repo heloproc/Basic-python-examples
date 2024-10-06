029a2e5219c6f50d3810229bd05ac83310c5445a
Hwa Young <admin@repo.git>
2024-10-06 00:31:30 -0700

    A py program performing right justify by 70pts

---
+++ justify.py
@@ -0,0 +1,13 @@
+def right_justify(s):
+    """Right justifies a string to 70 characters."""
+    l = []
+    for i in s:
+        l.append(i)
+
+    pos = 70 - len(l)  # Calculate number of spaces
+    justified_string = " " * pos + "".join(l)  # Create justified string
+    return justified_string  # Return the justified string
+
+
+inp = input("Enter a string: ")
+print(right_justify(inp))
