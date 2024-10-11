4f79cec03455d16ff8c963a549f65a9245f43801
Hwa Young <admin@repo.git>
2024-10-10 23:55:20 -0700

    demo of *args i.e. var len input

--- var_args_tuple.py
+++ var_args_tuple.py
@@ -5,14 +5,14 @@ def printall(*args):
 print args
 # The gather parameter can have any name you like, but args is conventional. Here’s how
 # the function works:
->>> printall(1, 2.0, '3')
+printall(1, 2.0, '3')
 (1, 2.0, '3')
 # The complement of gather is scatter. If you have a sequence of values and you want to pass
 # it to a function as multiple arguments, you can use the * operator. For example, divmod
 # takes exactly two arguments; it doesn’t work with a tuple:
->>> t = (7, 3)
->>> divmod(t)
+t = (7, 3)
+divmod(t)
 TypeError: divmod expected 2 arguments, got 1
 # But if you scatter the tuple, it works:
->>> divmod(*t)
+divmod(*t)
 (2, 1)
